5da79c5bd9e4be5a71907ecf3f0dc7fe3d0c3823
Daniel <raptor180122@mail.ru>
2021-03-07 15:38:55 +0700

    Add severity decorators for test_main_page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -19,55 +19,63 @@ def test_user_can_go_to_models_page(driver):
     page.accept_cookie()
     page.open_models_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_projects_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_projects_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_clients_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_clients_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_contacts_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_contacts_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_development_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_development_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_qa_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_qa_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_devops_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_devops_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_squads_product_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_squads_product_page()
 
-
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_main_page_by_clicking_to_logo(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_main_page_by_click_to_logo()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_open_get_in_touch_form(driver):
     page = MainPage(driver, LINK)
     page.open()
@@ -75,37 +83,42 @@ def test_user_can_open_get_in_touch_form(driver):
     page.open_get_in_touch_form()
     page.close_get_in_touch_form()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_user_can_go_to_team_extension_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_team_extension_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_open_why_sibedge_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_why_sibedge_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_open_press_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_press_page()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_open_blog_page(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_blog_page()
 
-
+@allure.severity(allure.severity_level.CRITICAL)
 def test_open_subscribe_to_us_form(driver):
     page = MainPage(driver, LINK)
     page.open()
     page.accept_cookie()
     page.open_subscribe_to_us_form()
 
+@allure.severity(allure.severity_level.CRITICAL)
 def test_open_sitemap_page(driver):
     page = MainPage(driver, LINK)
     page.open()
