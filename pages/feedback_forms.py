eba339a427fcf9e76cdbbf7179498767d6306e56
Daniel <raptor180122@mail.ru>
2021-03-18 15:08:07 +0700

    added IMAP assertions in to all tests

--- pages/feedback_forms.py
+++ pages/feedback_forms.py
@@ -26,19 +26,20 @@ LINKS_DICT = {
     "pitfalls_article": "https://dev.sibedge.com/article/major-pitfalls/",
     "cto_article": "https://dev.sibedge.com/article/ctos-reshape-it-priorities-to-overcome-crisis-mode/",
 }
+"""This function returns True, if email body contains correct "name_id" value."""
 
-def get_email(name_value):
+
+def get_email(name_id):
     imap_user = "test4site@sibedge.com"
     imap_password = "Hup28813"
 
     with MailBox('outlook.office365.com').login(imap_user, imap_password) as mailbox:
         for message in mailbox.fetch(limit=1, reverse=True):
             email_body = message.text
-            if name_value in email_body:
-                return True
-            else:
-                return False
-            #assert name_value in email_body, "Unique name in not presented"
+
+            if name_id in email_body:
+                return bool
+            return "No 'name_id' value in email body"
 
 class FeedBackForms(BasePage):
 
@@ -53,9 +54,9 @@ class FeedBackForms(BasePage):
                                  ".cookie-alert__button.cookie-alert__button--agree.js--cookie-alert-allow").click()
 
     def open_and_send_write_to_us_form(self):
-        name = "TestWriteToUs"
+        name_id = "TestWriteToUs"
         self.driver.find_element(By.CSS_SELECTOR, MainPageLocators.HEADER_FORM_BUTTON).click()
-        self.driver.find_element(By.ID, WriteToUsFormLocators.NAME_FIELD).send_keys(name)
+        self.driver.find_element(By.ID, WriteToUsFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.ID, WriteToUsFormLocators.EMAIL_FIELD).send_keys("testemail@email.com")
         self.driver.find_element(By.ID, WriteToUsFormLocators.PHONE_FIELD).send_keys("+79513454323")
         self.driver.find_element(By.ID, WriteToUsFormLocators.MESSAGE_FIELD).send_keys("Hello. Have a good day!")
@@ -67,10 +68,11 @@ class FeedBackForms(BasePage):
 
         assert thank_you_notice != 0
         time.sleep(3)
-        assert bool(get_email(name)) != False, "Email have no 'name' in body"
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_subscribe_to_us_form(self):
-        self.driver.find_element(By.ID, SubscribeFormLocators.NAME_FIELD).send_keys('TestName')
+        name_id = "TestSubscribeToUs"
+        self.driver.find_element(By.ID, SubscribeFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.ID, SubscribeFormLocators.EMAIL_FIELD).send_keys("testemail@email.com")
         self.driver.find_element(By.ID, SubscribeFormLocators.MESSAGE_FIELD).send_keys("Hello. Have a good day!")
         time.sleep(1)
@@ -81,11 +83,14 @@ class FeedBackForms(BasePage):
         )
 
         assert thank_you_notice != 0
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_main_page_form(self):
+        name_id = "TestMainPage"
         self.driver.get(LINKS_DICT["main_page"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys("Testname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.PHONE_FIELD).send_keys("+79994512345")
@@ -93,11 +98,14 @@ class FeedBackForms(BasePage):
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_models_page_form(self):
+        name_id = "TestModelsPage"
         self.driver.get(LINKS_DICT["models_page"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys("Testname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.PHONE_FIELD).send_keys("+79994512345")
@@ -105,11 +113,14 @@ class FeedBackForms(BasePage):
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_about_us_page_form(self):
+        name_id = "TestAboutUs"
         self.driver.get(LINKS_DICT["about_us_page"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys("Testname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.PHONE_FIELD).send_keys("+79994512345")
@@ -117,11 +128,14 @@ class FeedBackForms(BasePage):
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_contacts_page_form(self):
+        name_id = "TestContacts"
         self.driver.get(LINKS_DICT["contacts_page"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys("Testname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.PHONE_FIELD).send_keys("+79994512345")
@@ -129,21 +143,27 @@ class FeedBackForms(BasePage):
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_blog_page_form(self):
+        name_id = "TestBlog"
         self.driver.get(LINKS_DICT["blog_page"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         time.sleep(1)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_development_service_form(self):
+        name_id = "TestDevelopmentService"
         self.driver.get(LINKS_DICT["service_development_page"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys("Testname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.COMPANY_FIELD).send_keys("Test-Company")
@@ -152,13 +172,16 @@ class FeedBackForms(BasePage):
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
-    #other 3 services
+    # other 3 services
 
     def open_and_send_agile_article_form(self):
+        name_id = "TestAgileArticle"
         self.driver.get(LINKS_DICT["agile_article"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys("Testname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.PHONE_FIELD).send_keys("+79994512345")
@@ -167,22 +190,28 @@ class FeedBackForms(BasePage):
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_pitfalls_article_form(self):
+        name_id = "TestPitfallsArticle"
         self.driver.get(LINKS_DICT["pitfalls_article"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys("Testname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         time.sleep(1)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
 
     def open_and_send_cto_article_form(self):
+        name_id = "TestCtoArticle"
         self.driver.get(LINKS_DICT["cto_article"])
         self.accept_cookie()
-        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys("Testname")
+        self.driver.find_element(By.NAME, BaseFeedbackFormLocators.NAME_FIELD).send_keys(name_id)
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.LAST_NAME_FIELD).send_keys("Testlastname")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.EMAIL_FIELD).send_keys("test@email.com")
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.PHONE_FIELD).send_keys("+79994512345")
@@ -191,3 +220,5 @@ class FeedBackForms(BasePage):
         self.driver.find_element(By.NAME, BaseFeedbackFormLocators.SEND_BUTTON).click()
 
         assert "success" in self.driver.current_url, "No 'success' in URL"
+        time.sleep(3)
+        assert bool(get_email(name_id)) is not False, f"Email have no correct id -  {name_id} in body"
