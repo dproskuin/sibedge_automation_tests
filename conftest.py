886daac1aad6c2b6125a293c801a44202152735b
Daniel <raptor180122@mail.ru>
2021-03-19 12:48:46 +0700

    Code style: quotes fix 2

--- conftest.py
+++ conftest.py
@@ -3,41 +3,41 @@ from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_language', action='store', default='en',
+    parser.addoption("--browser_language", action="store", default="en",
                      help="Choose language: ru or en")
 
 
-@pytest.fixture(autouse=True, scope='function')
+@pytest.fixture(autouse=True, scope="function")
 def driver(request):
     browser_language = request.config.getoption("browser_language")
 
     if browser_language == "ru":
 
-        print('\nStart browser for test...')
+        print("\nStart browser for test...")
         options = webdriver.ChromeOptions()
-        options.add_argument('--lang=ru')
-        options.add_argument('--start-maximized')
-        options.add_argument('--user-agent=test_agent')
-        # options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--lang=ru")
+        options.add_argument("--start-maximized")
+        options.add_argument("--user-agent=test_agent")
+        # options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(options=options)
 
     elif browser_language == "en":
 
-        print('\nStart browser for test...')
+        print("\nStart browser for test...")
         options = webdriver.ChromeOptions()
-        options.add_argument('--lang=en')
-        options.add_argument('--start-maximized')
-        options.add_argument('--user-agent=test_agent')
-        # options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--lang=en")
+        options.add_argument("--start-maximized")
+        options.add_argument("--user-agent=test_agent")
+        # options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(options=options)
 
     else:
         raise pytest.UsageError("--browser_language is invalid, supported languages: en, ru")
 
     yield driver
-    print('\nquit browser..')
+    print("\nquit browser..")
     driver.quit()
